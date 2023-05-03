a0f28fbfb9e5d63302b6712f632d38206059311a
Matvei <matvesch@protonmail.com>
2023-05-03 15:31:08 +0300

    fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,26 @@ import 'normalize.css/normalize.css';
 import '@styles/globals.scss';
 import type { AppProps } from 'next/app';
 import { Rubik } from 'next/font/google';
+import localFont from 'next/font/local';
 
 const rubik = Rubik({
   subsets: ['latin', 'hebrew', 'cyrillic'],
   weight: ['400', '300'],
+});
+
+const gillSans = localFont({
+  src: [
+    {
+      path: './fonts/GillSansWoff2/GillSansC.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: './fonts/GillSansWoff2/GillSansLightC.woff2',
+      weight: '300',
+      style: 'normal',
+    },
+  ],
 });
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -13,7 +29,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <style jsx global>{`
         html {
-          font-family: ${rubik.style.fontFamily};
+          font-family: ${gillSans.style.fontFamily}, ${rubik.style.fontFamily};
         }
       `}</style>
       <Component {...pageProps} />
