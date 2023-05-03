6cc767731ea49a3096504469a871e1cfe8f53a07
Matvei <matvesch@protonmail.com>
2023-05-03 15:01:20 +0300

    added font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,22 @@
 import 'normalize.css/normalize.css';
 import '@styles/globals.scss';
 import type { AppProps } from 'next/app';
+import { Rubik } from 'next/font/google';
+
+const rubik = Rubik({
+  subsets: ['latin', 'hebrew', 'cyrillic'],
+  weight: ['400', '300'],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${rubik.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  );
 }
