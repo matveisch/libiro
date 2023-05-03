266d3c775b5efb1187c33963e6aed3a6f743b592
Matvei <matvesch@protonmail.com>
2023-05-03 14:22:07 +0300

    fixes

--- next.config.js
+++ next.config.js
@@ -12,14 +12,4 @@ const nextConfig = {
   },
 };
 
-// module.exports = nextConfig;
-
-module.exports = (phase, { defaultConfig }) => {
-  if ('sassOptions' in defaultConfig) {
-    defaultConfig['sassOptions'] = {
-      includePaths: ['./src'],
-      prependData: `@import "~@styles/variables.scss";`,
-    };
-  }
-  return defaultConfig;
-};
+module.exports = nextConfig;
