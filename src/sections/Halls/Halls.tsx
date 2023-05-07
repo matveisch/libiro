021be1e330cd78041c304ff88132b540c6607fa9
mistifikatorcat <92595353+mistifikatorcat@users.noreply.github.com>
2023-05-07 21:05:48 +0300

    fix hover states
    applying styles next

--- src/sections/Halls/Halls.tsx
+++ src/sections/Halls/Halls.tsx
@@ -13,39 +13,38 @@ export default function Halls() {
       <div className={styles.halls__container}>
         <div className={styles.halls__types}>
           <div className={styles.halls__item}>
-            <h2 className={styles.halls__itemTitle} onMouseEnter={() => setHover(0)} onMouseLeave={() => setHover(-1)}>
+            <h2 className={styles.halls__itemTitle} onMouseEnter={() => setHover(0)} >
               Main restaurant hall
               </h2>
           </div>
           <div className={styles.halls__item}>
-            <h2 className={styles.halls__itemTitle}  onMouseEnter={() => setHover(1)} onMouseLeave={() => setHover(-1)}>Private room</h2>
+            <h2 className={styles.halls__itemTitle}  onMouseEnter={() => setHover(1)} >Private room</h2>
           </div>
           <div className={styles.halls__item}>
-            <h2 className={styles.halls__itemTitle } onMouseEnter={() => setHover(2)} onMouseLeave={() => setHover(-1)}>Events gallery</h2>
+            <h2 className={styles.halls__itemTitle } onMouseEnter={() => setHover(2)}>Events gallery</h2>
           </div>
-
-          {hallsInfo.map((hall, index) => (
+          </div>
+         
             <div
-            key={index}
-            className={`styles.halls__infoScreen ${hover === index && 'active'}`}>
+           
+            className={`styles.halls__infoScreen}`}>
             <div className={styles.halls__infoScreenWrap}>
               <div className={styles.halls__infoScreenItem}>
                 <h5 className={styles.halls__number}>
-                    {hall.number}
+                    {hallsInfo[hover].number}
                 </h5>
                 <div className={styles.halls__capacity}>
-                  <h4 className={styles.halls__capacityText}>{hall.seating}</h4>
-                  <h4 className={styles.halls__capacityText}>{hall.squarage}</h4>
+                  <h4 className={styles.halls__capacityText}>{hallsInfo[hover].seating}</h4>
+                  <h4 className={styles.halls__capacityText}>{hallsInfo[hover].squarage}</h4>
                 </div>
                 <div className={styles.halls__descriptionWrap}>
-                  <p className={styles.halls__description}>{hall.description}</p>
+                  <p className={styles.halls__description}>{hallsInfo[hover].description}</p>
                 </div>
                 
               </div>
             </div>
           </div>
-          ))}
-        </div>
+          
       </div>
     </section>
     );
