d0842476e29d3f1ec7ac13ee4d684b5727def0be
Tommy Lidkovsky <armkudr@gmail.com>
2023-05-08 08:04:39 +0300

    smotry

--- src/sections/Halls/Halls.tsx
+++ src/sections/Halls/Halls.tsx
@@ -4,38 +4,73 @@ import styles from './Halls.module.scss';
 import { hallsInfo } from './HallsInfo';
 
 export default function Halls() {
-  const [hover, setHover] = React.useState(-1);
-  const [active, setActive] = React.useState(-1);
+  const [hover, setHover] = React.useState(0);
+  const [active, setActive] = React.useState(0);
+  const [test, setTest] = React.useState(2);
 
   return (
     <section className={styles.halls}>
       <div className={styles.halls__container}>
         {/* <div className={styles.halls__typesWrapper}> */}
-        <div className={styles.halls__types} onMouseLeave={() => setActive(-1)}>
-          <div className={`${styles.halls__item} ${active === 0 || (hover === 0 && active === -1) ? styles.active : ''}`}>
-            
+        <div className={styles.halls__types}>
+          {hallsInfo.map((hall, index) => {
+            return (
+              <div
+                className={`${styles.halls__item} ${
+                  hover === index ? styles.active : ''
+                }`}
+              >
+                <h2
+                  className={`${styles.halls__itemTitle} ${
+                    hover === index ? styles.active : ''
+                  }`}
+                  onMouseOver={() => setHover(index)}
+                  onClick={() => setHover(index)}
+                >
+                  {hall.name}
+                </h2>
+              </div>
+            );
+          })}
+          {/* <div
+            className={`${styles.halls__item} ${
+              active === 0 || (hover === 0 && active === -1)
+                ? styles.active
+                : ''
+            }`}
+          >
             <h2
               className={styles.halls__itemTitle}
-              onMouseOver={() => setHover(0) }
+              onMouseOver={() => setHover(0)}
               onMouseLeave={() => setHover(-1)}
               onClick={() => setActive(active === 0 ? -1 : 0)}
             >
               Main restaurant hall
             </h2>
-            
           </div>
-          <div className={`${styles.halls__item} ${active === 1 || (hover === 1 && active === -1) ? styles.active : ''}`}>
+          <div
+            className={`${styles.halls__item} ${
+              active === 1 || (hover === 1 && active === -1)
+                ? styles.active
+                : ''
+            }`}
+          >
             <h2
               className={styles.halls__itemTitle}
               onMouseOver={() => setHover(1)}
               onMouseLeave={() => setHover(-1)}
               onClick={() => setActive(active === 0 ? -1 : 0)}
-
             >
               Private room
             </h2>
           </div>
-          <div className={`${styles.halls__item} ${active === 2 || (hover === 2 && active === -1) ? styles.active : ''}`}>
+          <div
+            className={`${styles.halls__item} ${
+              active === 2 || (hover === 2 && active === -1)
+                ? styles.active
+                : ''
+            }`}
+          >
             <h2
               className={styles.halls__itemTitle}
               onMouseOver={() => setHover(2)}
@@ -44,10 +79,9 @@ export default function Halls() {
             >
               Events gallery
             </h2>
-          </div>
+          </div> */}
         </div>
-        {/* </div> */}
-        <div className={`styles.halls__infoScreen}`}>
+        <div className={`${styles.halls__infoScreen}`}>
           <div className={styles.halls__infoScreenWrap}>
             <div className={styles.halls__infoScreenItem}>
               <h5 className={styles.halls__number}>
@@ -61,11 +95,10 @@ export default function Halls() {
                   {hallsInfo[hover].squarage}
                 </h4>
               </div>
-              
-                <p className={styles.halls__description}>
-                  {hallsInfo[hover].description}
-                </p>
-             
+
+              <p className={styles.halls__description}>
+                {hallsInfo[hover].description}
+              </p>
             </div>
           </div>
         </div>
